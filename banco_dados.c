e06ed29b7637b4fd3a98a30e17bb9b9e38ac81f8
M4theusD3V <156040328+M4theusD3V@users.noreply.github.com>
2024-01-08 23:24:29 -0300

    Upload de projeto
    Adição da opção de saída do sistema.

--- banco_dados.c
+++ banco_dados.c
@@ -128,6 +128,7 @@ int main() //entrada inicial do sistema
    printf("\t1 - Registrar nomes\n");
    printf("\t2 - Consultar nomes\n");
    printf("\t3 - Deletar nomes\n\n");
+   printf("\t4 - Sair do Sistema\n\n");
    printf("opção: "); //fim do menu
    
    scanf("%d",&opcao); //armazenando a escolha do cliente
@@ -146,6 +147,11 @@ int main() //entrada inicial do sistema
    	
    	case 3:
   	deletar();
+   	break;
+   	
+   	case 4:
+   	printf("Obrigado por utilizar o sistema! :)\n");
+   	return 0;
    	break;
    	
    	default:
